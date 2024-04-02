e3fb732a8f1d6c3271142ddf5f5f0db51cffcd0a
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-04-02 15:45:41 +1300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.detekt) apply false
+
+    alias(libs.plugins.dokka)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,51 @@
+import com.android.build.api.dsl.SettingsExtension
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version("3.16")
+    id("com.android.settings") version("8.3.1")
+}
+
+rootProject.name = "kotlin-datatypes"
+
+include(
+    ":capacity",
+    ":percentage",
+    ":temperature"
+)
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        if (System.getenv("CI") == "true") {
+            termsOfServiceAgree = "yes"
+            isUploadInBackground = false
+        }
+    }
+}
+
+configure<SettingsExtension> {
+    buildToolsVersion = "34.0.0"
+    compileSdk = 34
+    minSdk = 28
+}
