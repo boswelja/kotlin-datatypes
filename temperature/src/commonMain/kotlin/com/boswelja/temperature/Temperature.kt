8621a36674fc5a47ae9bd4684c961cf0cfc6739c
Jack Boswell (boswelja) <boswelja@outlook.com>
2024-11-13 20:42:31 +1100

    Use Number for Temperature

--- temperature/src/commonMain/kotlin/com/boswelja/temperature/Temperature.kt
+++ temperature/src/commonMain/kotlin/com/boswelja/temperature/Temperature.kt
@@ -76,103 +76,34 @@ value class Temperature internal constructor(private val kelvin: BigDecimal) : C
     @Suppress("unused")
     companion object {
 
-        private fun Int.toTemperature(unit: TemperatureUnit): Temperature =
-            Temperature(unit.toKelvin(this.toBigDecimal()))
-        private fun Long.toTemperature(unit: TemperatureUnit): Temperature =
-            Temperature(unit.toKelvin(this.toBigDecimal()))
-        private fun Float.toTemperature(unit: TemperatureUnit): Temperature =
-            Temperature(unit.toKelvin(this.toBigDecimal()))
-        private fun Double.toTemperature(unit: TemperatureUnit): Temperature =
-            Temperature(unit.toKelvin(this.toBigDecimal()))
+        fun Number.toTemperature(unit: TemperatureUnit): Temperature {
+            return when (this) {
+                is Byte -> Temperature(unit.toKelvin(this.toBigDecimal()))
+                is Double -> Temperature(unit.toKelvin(this.toBigDecimal()))
+                is Float -> Temperature(unit.toKelvin(this.toBigDecimal()))
+                is Int -> Temperature(unit.toKelvin(this.toBigDecimal()))
+                is Long -> Temperature(unit.toKelvin(this.toBigDecimal()))
+                is Short -> Temperature(unit.toKelvin(this.toBigDecimal()))
+                else -> {
+                    // Best-effort conversion
+                    Temperature(unit.toKelvin(this.toDouble().toBigDecimal()))
+                }
+            }
+        }
 
-        // region kelvin
-        /** Converts an [Int] representation of kelvin to a [Temperature]. */
-        val Int.kelvin: Temperature
-            get() = toTemperature(TemperatureUnit.KELVIN)
+        /** Converts a [Number] representation of kelvin to a [Temperature]. */
+        val Number.kelvin: Temperature get() = toTemperature(TemperatureUnit.KELVIN)
 
-        /** Converts an [Long] representation of kelvin to a [Temperature]. */
-        val Long.kelvin: Temperature
-            get() = toTemperature(TemperatureUnit.KELVIN)
+        /** Converts a [Number] representation of celsius to a [Temperature]. */
+        val Number.celsius: Temperature get() = toTemperature(TemperatureUnit.CELSIUS)
 
-        /** Converts an [Float] representation of kelvin to a [Temperature]. */
-        val Float.kelvin: Temperature
-            get() = toTemperature(TemperatureUnit.KELVIN)
+        /** Converts a [Number] representation of fahrenheit to a [Temperature]. */
+        val Number.fahrenheit: Temperature get() = toTemperature(TemperatureUnit.FAHRENHEIT)
 
-        /** Converts an [Double] representation of kelvin to a [Temperature]. */
-        val Double.kelvin: Temperature
-            get() = toTemperature(TemperatureUnit.KELVIN)
-        // endregion
+        /** Converts a [Number] representation of rankine to a [Temperature]. */
+        val Number.rankine: Temperature get() = toTemperature(TemperatureUnit.RANKINE)
 
-        // region celsius
-        /** Converts an [Int] representation of celsius to a [Temperature]. */
-        val Int.celsius: Temperature
-            get() = toTemperature(TemperatureUnit.CELSIUS)
-
-        /** Converts an [Long] representation of celsius to a [Temperature]. */
-        val Long.celsius: Temperature
-            get() = toTemperature(TemperatureUnit.CELSIUS)
-
-        /** Converts an [Float] representation of celsius to a [Temperature]. */
-        val Float.celsius: Temperature
-            get() = toTemperature(TemperatureUnit.CELSIUS)
-
-        /** Converts an [Double] representation of celsius to a [Temperature]. */
-        val Double.celsius: Temperature
-            get() = toTemperature(TemperatureUnit.CELSIUS)
-        // endregion
-
-        // region fahrenheit
-        /** Converts an [Int] representation of fahrenheit to a [Temperature]. */
-        val Int.fahrenheit: Temperature
-            get() = toTemperature(TemperatureUnit.FAHRENHEIT)
-
-        /** Converts an [Long] representation of fahrenheit to a [Temperature]. */
-        val Long.fahrenheit: Temperature
-            get() = toTemperature(TemperatureUnit.FAHRENHEIT)
-
-        /** Converts an [Float] representation of fahrenheit to a [Temperature]. */
-        val Float.fahrenheit: Temperature
-            get() = toTemperature(TemperatureUnit.FAHRENHEIT)
-
-        /** Converts an [Double] representation of fahrenheit to a [Temperature]. */
-        val Double.fahrenheit: Temperature
-            get() = toTemperature(TemperatureUnit.FAHRENHEIT)
-        // endregion
-
-        // region rankine
-        /** Converts an [Int] representation of rankine to a [Temperature]. */
-        val Int.rankine: Temperature
-            get() = toTemperature(TemperatureUnit.RANKINE)
-
-        /** Converts an [Long] representation of rankine to a [Temperature]. */
-        val Long.rankine: Temperature
-            get() = toTemperature(TemperatureUnit.RANKINE)
-
-        /** Converts an [Float] representation of rankine to a [Temperature]. */
-        val Float.rankine: Temperature
-            get() = toTemperature(TemperatureUnit.RANKINE)
-
-        /** Converts an [Double] representation of rankine to a [Temperature]. */
-        val Double.rankine: Temperature
-            get() = toTemperature(TemperatureUnit.RANKINE)
-        // endregion
-
-        // region réaumur
-        /** Converts an [Int] representation of réaumur to a [Temperature]. */
-        val Int.reaumur: Temperature
-            get() = toTemperature(TemperatureUnit.REAUMUR)
-
-        /** Converts an [Long] representation of réaumur to a [Temperature]. */
-        val Long.reaumur: Temperature
-            get() = toTemperature(TemperatureUnit.REAUMUR)
-
-        /** Converts an [Float] representation of réaumur to a [Temperature]. */
-        val Float.reaumur: Temperature
-            get() = toTemperature(TemperatureUnit.REAUMUR)
-
-        /** Converts an [Double] representation of réaumur to a [Temperature]. */
-        val Double.reaumur: Temperature
-            get() = toTemperature(TemperatureUnit.REAUMUR)
-        // endregion
+        /** Converts a [Number] representation of réaumur to a [Temperature]. */
+        val Number.reaumur: Temperature get() = toTemperature(TemperatureUnit.REAUMUR)
     }
 }
